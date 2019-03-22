1c89644d976028f75fd94e95bde930a479e5078d
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2019-03-22 11:02:12 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React,{ useState } from 'react';
+import ReactDOM from 'react-dom';
+
+import * as serviceWorker from './serviceWorker';
+
+
+
+const App = (props) =>{
+    const [count , setCount ] = useState(props.count);
+
+    const increment = () =>{
+      setCount(count + 1);
+    }
+
+    const decrement = () =>{
+      setCount(count - 1);
+    }
+
+    const reset = () => {
+        setCount (props.count);
+    }
+
+    return (
+        <div>
+        <p>The Current Count Is {count}</p>
+        <button onClick={increment}>+1</button>
+        <button onClick={reset}>reset</button>
+        <button onClick={decrement}>-1</button>
+        </div>
+    )
+}
+
+App.defaultProps = {
+    count: 0
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
